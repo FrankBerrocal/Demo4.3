6b4dd2669fa7ca3b7678fde5ac9fc907ff1582fa
Frank <f.berrocalazofeif887@mybvc.ca>
2023-03-05 17:46:11 -0600

    Creation of menu bar.

--- app/src/main/java/com/example/demo43/MainActivity.java
+++ app/src/main/java/com/example/demo43/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.demo43;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        when(item.getItemId()){
+            R.id.mi_contact->Toast.makeText(this, "You clicked on add contact", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
